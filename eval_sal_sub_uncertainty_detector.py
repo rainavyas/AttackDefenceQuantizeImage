9866cf0641e25965cf5a838f849531b93f1bd37d
Vyas Raina <sayvaniar@gmail.com>
2021-11-29 16:26:18 +0000

    bug

--- eval_sal_sub_uncertainty_detector.py
+++ eval_sal_sub_uncertainty_detector.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     adv_uncertainties = ensemble_uncertainties_classification(attacked_probs)
 
     # For each uncertainty measure report F1-score
-    labels = [0]*imgs_original + [1]*imgs_attack
+    labels = [0]*len(imgs_original) + [1]*len(imgs_attack)
 
     for measure in original_uncertainties.keys():
         original = original_uncertainties[measure]
